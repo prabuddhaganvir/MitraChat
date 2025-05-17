6f34f48b553dfc83055673106930e2f5850da63a
prabuddhaganvir <prabuddhapg12@gmail.com>
2025-05-17 17:06:20 +0530

    DB Connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
  import express from "express"
  import authRoute from "./routes/auth.route.js"
+ import dotenv from "dotenv"
+ import { connectDB } from "./lib/db.js"
 
+ dotenv.config()
+ const PORT = process.env.PORT
  const app = express()
-
+ 
   app.use("/api/auth", authRoute)
 
-  app.listen(5001, ()=>{
-    console.log("server is runnig on port 5001");
-    
+  app.listen(PORT, ()=>{
+    console.log("server is runnig on port:"+ PORT);
+    connectDB()
  })
