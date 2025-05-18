0870447b26e67868d3c1f5496aaf934cedcdd8c2
prabuddhaganvir <prabuddhapg12@gmail.com>
2025-05-18 14:21:31 +0530

    completed Backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
  import express from "express"
  import authRoute from "./routes/auth.route.js"
+ import messageRoute from "./routes/message.route.js"
  import dotenv from "dotenv"
  import { connectDB } from "./lib/db.js"
  import cookieParser from "cookie-parser"
@@ -10,7 +11,12 @@
  
   app.use(express.json())
   app.use(cookieParser())
+
+
   app.use("/api/auth", authRoute)
+  app.use("/api/messages", messageRoute)
+
+
 
   app.listen(PORT, ()=>{
     console.log("server is runnig on port:"+ PORT);
